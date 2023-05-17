bae6594bc9d07dc1e45c9c114c0905309039ec03
sanya <sanya2003ogorodov@gmail.com>
2023-05-18 01:06:10 +0700

    feat: dont do extra stuff if src-size == dest-size

--- src/stbresize.c
+++ src/stbresize.c
@@ -16,9 +16,9 @@
 void resize_usage(char* prog, int resize_height, int resize_width, float ratio, int quality)
 {
     printf("stb-resize version %s.\n", RESIZE_VERSION);
-    printf("usage: %s [options] image_in.xxx out.xxx\n", prog);
+    printf("usage: %s [options] image_in.xxx image_out.xxx\n", prog);
     printf("formats: png, jpg(jpeg)\n");
-    printf("method: bicubic (with Gauss prefilter if ratio < 1.0)\n");
+    printf("method: bicubic (with Gauss prefilter for downsize)\n");
     printf("options:\n");
     printf("  -H px     resize height (default %d)\n", resize_height);
     printf("  -W px     resize width (default %d)\n", resize_width);
@@ -27,21 +27,46 @@ void resize_usage(char* prog, int resize_height, int resize_width, float ratio, 
     printf("  -h        show this help message and exit\n");
 }
 
+void resize(unsigned char *src, int height, int width, int channels, int resize_height, int resize_width, unsigned char *res)
+{
+    int gaussrx = 0, gaussry = 0;
+    float vgauss;
+
+    // Use Gauss prefilter if image downsizes only
+    if ((resize_height < height) || (resize_width < width))
+    {
+        printf("prefilter: Gauss\n");
+        gaussry = (resize_height < height) ? (0.5f * (float)height / (float)resize_height) : 0.0f;
+        gaussrx = (resize_width < width) ? (0.5f * (float)width / (float)resize_width) : 0.0f;
+        vgauss = GaussBlurFilter(src, height, width, channels, gaussry, gaussrx);
+        printf("  gauss-value: %f\n", vgauss);
+    }
+    else
+    {
+        printf("prefilter: none\n");
+    }
+
+    printf("resize: %dx%d:%d\n", resize_width, resize_height, channels);
+
+    printf("method: bicubic\n");
+    ResizeImageBiCubic(src, height, width, channels, resize_height, resize_width, res);
+}
+
 int main(int argc, char **argv)
 {
     int height, width, channels, y, x, d;
     int resize_height = 0, resize_width = 0;
+    bool resizing = true;
     float ratio = 1.0f;
     int quality = 100;
-    int gaussrx = 0, gaussry = 0;
-    float vgauss;
 
     int fhelp = 0;
     int opt;
-    size_t szorig, szdest, ki, kd;
-    unsigned char *data = NULL, *resize_data = NULL;
+    size_t ki, kd;
+    unsigned char *data = NULL, *resize_data = NULL, *save_data = NULL;
     stbi_uc *img = NULL;
 
+    // Getting CLI options
     while ((opt = getopt(argc, argv, ":H:W:r:q:h")) != -1)
     {
         switch(opt)
@@ -49,9 +74,11 @@ int main(int argc, char **argv)
         case 'H':
             resize_height = atoi(optarg);
             break;
+
         case 'W':
             resize_width = atoi(optarg);
             break;
+
         case 'r':
             ratio = atof(optarg);
             if (ratio < 0.0f)
@@ -61,6 +88,7 @@ int main(int argc, char **argv)
                 return 1;
             }
             break;
+
         case 'q':
             quality = atoi(optarg);
             if (quality <= 0 || 100 < quality)
@@ -70,13 +98,16 @@ int main(int argc, char **argv)
                 return 1;
             }
             break;
+
         case 'h':
             fhelp = 1;
             break;
+
         case ':':
             fprintf(stderr, "ERROR: option needs a value\n");
             return 2;
             break;
+
         case '?':
             fprintf(stderr, "ERROR: unknown option: %c\n", optopt);
             return 3;
@@ -91,7 +122,14 @@ int main(int argc, char **argv)
     const char *src_name = argv[optind];
     const char *dst_name = argv[optind + 1];
 
+    // Check if user doesn't resize image in fact
+    if ((ratio == 1.f && resize_width == 0 && resize_height == 0) ||
+        (resize_width == width && resize_height == height))
+    {
+        resizing = false;
+    }
 
+    // Load image from file
     printf("Load: %s\n", src_name);
     if (!(img = stbi_load(src_name, &width, &height, &channels, STBI_rgb_alpha)))
     {
@@ -99,6 +137,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // Allocate memory for image data
     printf("image: %dx%d:%d\n", width, height, channels);
     if (!(data = (unsigned char*)malloc(height * width * channels * sizeof(unsigned char))))
     {
@@ -106,6 +145,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // Get image data
     ki = 0;
     kd = 0;
     for (y = 0; y < height; y++)
@@ -121,62 +161,55 @@ int main(int argc, char **argv)
         }
     }
     stbi_image_free(img);
-    szorig = (size_t)height * width;
 
-    if ((resize_height == 0) && (resize_width == 0))
+    // Check if width or height must be resized by ratio
+    if (resize_height == 0)
     {
         resize_height = (int)((float)height * ratio + 0.5f);
+    }
+
+    if (resize_width == 0)
+    {
         resize_width = (int)((float)width * ratio + 0.5f);
+    }
+
+    // Making resize stuff if need only
+    if(resizing)
+    {
+        if ((resize_height == 0) || (resize_width == 0))
+        {
+            fprintf(stderr, "ERROR: bad target size %dx%d:%d\n", resize_width, resize_height, channels);
+            return 1;
+        }
+
+        if (!(resize_data = (unsigned char*)malloc(resize_height * resize_width * channels * sizeof(unsigned char))))
+        {
+            fprintf(stderr, "ERROR: failed to allocate memory\n");
+            return 2;
+        }
+
+        resize(data, height, width, channels, resize_height, resize_width, resize_data);
+
+        save_data = resize_data;
     }
     else
     {
-        if (resize_height == 0)
-        {
-            ratio = (float)resize_width / (float)width;
-            resize_height = (int)((float)height * ratio + 0.5f);
-        }
-        if (resize_width == 0)
-        {
-            ratio = (float)resize_height / (float)height;
-            resize_width = (int)((float)width * ratio + 0.5f);
-        }
-    }
-    if ((resize_height == 0) || (resize_width == 0))
-    {
-        fprintf(stderr, "ERROR: bad target size %dx%d:%d\n", resize_width, resize_height, channels);
-        return 1;
-    }
-    szdest = (size_t)resize_height * resize_width;
-
-    if ((resize_height < height) || (resize_width < width))
-    {
-        printf("prefilter: Gauss\n");
-        gaussry = (resize_height < height) ? (0.5f * (float)height / (float)resize_height) : 0.0f;
-        gaussrx = (resize_width < width) ? (0.5f * (float)width / (float)resize_width) : 0.0f;
-        vgauss = GaussBlurFilter(data, height, width, channels, gaussry, gaussrx);
-        printf("  gauss-value: %f\n", vgauss);
+        save_data = data;
+        printf("Saving with original size\n");
     }
 
-    printf("resize: %dx%d:%d\n", resize_width, resize_height, channels);
-    if (!(resize_data = (unsigned char*)malloc(resize_height * resize_width * channels * sizeof(unsigned char))))
-    {
-        fprintf(stderr, "ERROR: not use memmory\n");
-        return 2;
-    }
-
-    printf("method: bicubic\n");
-    ResizeImageBiCubic(data, height, width, channels, resize_height, resize_width, resize_data);
-
+    // Checking target format and saving image to file
     bool retval = false;
     if(strcmp(&dst_name[strlen(dst_name) - strlen(".jpg")], ".jpg") == 0)
     {
+        printf("Quality: %d%%\n", quality);
         printf("Save jpg: %s\n", dst_name);
-        retval = stbi_write_jpg(dst_name, resize_width, resize_height, channels, resize_data, quality);
+        retval = stbi_write_jpg(dst_name, resize_width, resize_height, channels, save_data, quality);
     }
     else if(strcmp(&dst_name[strlen(dst_name) - strlen(".png")], ".png") == 0)
     {
         printf("Save png: %s\n", dst_name);
-        retval = stbi_write_png(dst_name, resize_width, resize_height, channels, resize_data, resize_width * channels);
+        retval = stbi_write_png(dst_name, resize_width, resize_height, channels, save_data, resize_width * channels);
     }
     else
     {
@@ -189,7 +222,12 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    free(resize_data);
+    // Free allocated memory
+    if(resizing)
+    {
+        free(resize_data);
+    }
     free(data);
+
     return 0;
 }
