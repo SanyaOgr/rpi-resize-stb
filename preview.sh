01ef88d33e995f87c19cae8905c0a8a719af53e3
sanya <sanya2003ogorodov@gmail.com>
2023-05-25 12:26:33 +0700

    final works

--- preview.sh
+++ preview.sh
@@ -1,35 +1,63 @@
 #! /bin/bash
-# make from photo 340x240 preview with preffered file-size(in Kb)
-# parameter: photo file-size(Kb)
+# make 340x240 preview and 1024x768 photo with preffered file-sizes(in Kb)
+# parameter: [photo number] [preview-file-size] [photo-file-size]
 
-src=$1
-dst=prev-$src
-pref_size=$(($2 * 1024))
+num=$1
+src_dir="."
+src_name="$num.jpg"
+dst_dir="$HOME/thumbs"
+prev_name="$num-prev.jpg"
+photo_name="$num-main.jpg"
+
+src_path="$src_dir/$src_name"
+prev_path="$dst_dir/$prev_name"
+photo_path="$dst_dir/$photo_name"
+
+prev_good_size=$(($2 * 1024))
+photo_good_size=$(($3 * 1024))
 
 # resize
 
-echo "-- Resizing: 340x240, 80%"
+prev_qual=80
+photo_qual=80
 
-qual=80
-./stb-resize -W 340 -H 240 -q $qual $src $dst
-size=$(stat -c '%s' $dst)
+# echo "-- Resizing: 340x240, 80%"
+stb-resize -W 340 -H 240 -q $prev_qual $src_path $prev_path > /dev/null
 
-echo "-- File size: $size b"
+# echo "-- Resizing: 1024x768, 80%"
+stb-resize -W 1024 -H 768 -q $photo_qual $src_path $photo_path > /dev/null
 
-# decrease quality until size will be good
+prev_size=$(stat -c '%s' $prev_path)
+photo_size=$(stat -c '%s' $photo_path)
 
-while [ $size -gt $pref_size ]
+# echo "-- Preview size: $prev_size b"
+# echo "-- Photo size: $photo_size b"
+
+# decrease preview quality until size will be good
+
+while [ $prev_size -gt $prev_good_size ] && [ $prev_qual -gt 50 ]
 do
-    qual=$(( $qual - 10 ))
+    prev_qual=$(( $prev_qual - 10 ))
 
-    if [ $qual -lt 50 ]
-    then
-        break
-    fi
+    # echo "-- Decrease preview quality: $prev_qual %"
+    stb-resize -q $prev_qual $prev_path $prev_path > /dev/null
 
-    echo "-- Decrease quality: $qual %"
-    ./stb-resize -q $qual $dst $dst
-
-    size=$(stat -c '%s' $dst)
-    echo "-- File size: $size b"
+    prev_size=$(stat -c '%s' $prev_path)
+    # echo "-- Preview size: $prev_size b"
 done
+
+# decrease photo quality until size will be good
+
+while [ $photo_size -gt $photo_good_size ] && [ $photo_qual -gt 50 ]
+do
+    photo_qual=$(( $photo_qual - 10 ))
+
+    # echo "-- Decrease photo quality: $photo_qual %"
+    stb-resize -q $photo_qual $photo_path $photo_path > /dev/null
+
+    photo_size=$(stat -c '%s' $photo_path)
+    # echo "-- Photo size: $photo_size b"
+done
+
+echo "-- Preview: $prev_name (340x240 $prev_qual % $(($prev_size/1024)) Kb)"
+echo "-- Photo: $photo_name (1024x768 $photo_qual % $(($photo_size/1024)) Kb)"
